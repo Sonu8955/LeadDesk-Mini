743f31e696804ef3efa4aefc4cacf69d64637f26
Sonu8955 <kumarisonu6472@gmail.com>
2026-08-07 11:19:55 +0530

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://leaddesk-mini-6ufe.onrender.com/api",
 });
 
 export default API;
